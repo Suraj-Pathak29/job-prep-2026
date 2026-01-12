06eb4786b8e24346bcfd875ddc43237cc817edc9
Suraj-Pathak29 <sp29suraj@gmail.com>
2026-01-13 05:16:10 +0530

    Day 5: Validation, abort, error handlers added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , jsonify , request
+from flask import Flask , jsonify , request , abort
 from python.student_logic import students , add_student 
 
 app = Flask(__name__)
@@ -11,6 +11,45 @@ def students_handler():
     
     if request.method == "POST":
         data = request.get_json()
+        
+        #JSON existence check
+        if not data:
+              abort(400)
+        
+        #Required field validation
+        required_fields = ["id" , "name" , "age" , "marks"]
+        for field in required_fields:
+              if field not in data:
+                    return jsonify({"error":f"{field} is required"}),400
+    
+        #Type validation
+        if not isinstance(data["id"] , int):
+              return jsonify({"error":"id must be an integer"}),400
+        
+        #Type validation
+        if not isinstance(data["name"] , str):
+              return jsonify({"error":"name must be a string"}),400
+        
+        #Business rule validation
+        if not data["name"].strip():
+              return jsonify({"error":"name cannot be empty"}),400
+        
+        #Type validation
+        if not isinstance(data["age"] , int):
+              return jsonify({"error":"age must be an integer"}),400
+        
+        #Business rule validation
+        if data["age"] <= 0:
+              return jsonify({"error":"age must be positive"}),400
+        
+        #Type validation
+        if not isinstance(data["marks"] , int):
+              return jsonify({"error":"marks must be an integer"}),400
+        
+        #Business rule validation
+        if data["marks"] < 0 or data["marks"] > 100:
+              return jsonify({"error":"marks must be between 0 and 100"}),400
+
         add_student(
             students,
             data["id"],
@@ -30,5 +69,20 @@ def delete_student_api(s_id):
                    
             return jsonify({"error":"Student not found"}),404
               
+
+@app.errorhandler(400)
+def bad_request(error):
+      return jsonify({
+            "status":"error",
+            "message":"Bad request"}),400
+
+app.errorhandler(404)
+def not_found(error):
+      return jsonify({
+            "status":"error",
+            "message":"Resource not found"}),404
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
